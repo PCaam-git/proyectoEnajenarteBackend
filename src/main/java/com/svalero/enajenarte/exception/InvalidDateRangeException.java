293df20ee8a1c22a6df16c71439e7106ae9886d7
PCaam-git <pcaamano@outlook.es>
2026-06-02 17:37:15 +0200

    mejora los mensajes de validación de fechas en la actividades

--- src/main/java/com/svalero/enajenarte/exception/InvalidDateRangeException.java
+++ src/main/java/com/svalero/enajenarte/exception/InvalidDateRangeException.java
@@ -1,7 +1,12 @@
 package com.svalero.enajenarte.exception;
 
-public class InvalidDateRangeException extends Exception{
+public class InvalidDateRangeException extends Exception {
+
     public InvalidDateRangeException() {
         super("La fecha de confirmación debe ser anterior a la fecha de inicio");
+    }
+
+    public InvalidDateRangeException(String message) {
+        super(message);
     }
 }
